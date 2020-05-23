3cbad3ba1a94bbc018e5f2c842db83302481a194
ryoken nakayama <ryoken7090@gmail.com>
2020-05-23 17:37:52 +0900

    新規投稿機能と一覧表示機能の作成

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,4 +1,12 @@
 class BlogsController < ApplicationController
   def new
+    @blog = Blog.new
+  end
+  def create
+    @blog = Blog.create(content: params[:blog][:content])
+    redirect_to blogs_path
+  end
+  def index
+    @blogs = Blog.all
   end
 end
